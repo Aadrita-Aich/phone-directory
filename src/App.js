f00e6236ecfcb7eb2104633912ae035fb5084ffb
Aadrita Aich <aadritaaich@gmail.com>
2022-09-17 20:50:11 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Fragment } from 'react';
 import React, { Component } from 'react';
 class App extends Component {
   render() {
-    return (
-      <div>
-
-        Phone Directory
-      </div>
-    );
+      return (
+        <Fragment>
+           <div>
+              Phone Directory
+           </div>
+           <button>Add</button>
+           <div>
+              <span>Name</span>
+              <span>Phone</span>
+           </div>
+        </Fragment>
+     );
   }
 }
 export default App;
